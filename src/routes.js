404d869881e6d91e27e1ad086a9d8a8a8ff284ca
PokemoshkaBOX <131866226+PokemoshkaBOX@users.noreply.github.com>
2024-03-03 21:28:08 +0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "./pages/HomePages/HomePage";
 import Monitor from "./pages/Monitor/Monitor";
 import Faculty from "./pages/Faculty/Faculty";
 import Year from "./pages/Year/Year";
